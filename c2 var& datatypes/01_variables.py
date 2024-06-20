7946f98cd7e750ee8dec4dc6e8bea5ae4414f656
CHIRAG VERMA <iitcv58@gmail.com>
2024-06-20 09:40:51 +0530

    Add files via upload

---
+++ c2 var& datatypes/01_variables.py
@@ -0,0 +1,10 @@
+# here a and b are container 
+a = 1 
+
+b = 2 
+
+c = "chirag"
+
+d = 56.78
+
+print(c)
---
+++ c2 var& datatypes/04_operators.py
@@ -0,0 +1,38 @@
+# arithmetic operators 
+a = 56
+b=67
+c = a+ b
+print(c)
+
+# Assignment operators 
+a = 4-5 # assign 4-2 in a 
+print(a)
+b=8
+b += 5 # 13
+print(b)
+
+# comparision operator 
+d =5<4
+print(d) # false
+
+e =4!=5
+print(e) #true
+
+#logical operator
+f = 5>4 and 3>2 # true
+print(f)
+print("\n")
+print("Truth table for 'OR' operator")
+print("True or False is", True or False)
+print("True or True is", True or True)
+print("False or True is", False or True)
+print("False or False is", False or False)
+print("\n")
+print("Truth table for 'AND' operator")
+print("True and True is", True and True)
+print("True and False is", True and False)
+print("False and True is", False and True)
+print("False and False is", False and False)
+print("\n")
+print("print(not(True))")
+print(not(True))
